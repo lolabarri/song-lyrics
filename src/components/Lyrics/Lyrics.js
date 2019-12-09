c67b3d22839e9747f26ab7d26b51b8a2f737b1fa
Leticia Olábarri <leticiaolabarri@yahoo.es>
2019-12-09 18:46:38 +0100

    A few empty components created

---
+++ src/components/Lyrics/Lyrics.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const lyrics = () => {
+  return(
+    <div>
+      <h2>Título de la canción</h2>
+      <p>Letra de la canción</p>
+    </div>
+  )
+}
+
+export default lyrics;
---
+++ src/components/TypeCard/TypeCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const typeCard = () => {
+  return(
+    <div></div>
+  )
+}
+
+export default typeCard;
