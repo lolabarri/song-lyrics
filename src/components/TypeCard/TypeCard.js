01c5f68fa6eb5ce402ab290b79156c3a15fa5274
Leticia Olábarri <leticiaolabarri@yahoo.es>
2019-12-18 20:38:53 +0100

    Changed font family and some colours

--- src/components/TypeCard/TypeCard.js
+++ src/components/TypeCard/TypeCard.js
@@ -2,9 +2,10 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledCard = styled.div`
-background-color: #61dafb;
+background-color: white;
 padding: 1%;
 border-radius: 10px;
+border: 2px solid #014687;
 width: 80%
 font-size: calc(10px + 2vmin);
 margin: 2%
